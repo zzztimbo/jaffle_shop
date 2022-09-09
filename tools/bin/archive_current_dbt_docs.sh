ac3c57e004e1a1d785353ccf4e0fde88ff98eb50
Tim Chan <tim@chan.net>
2022-09-08 17:35:55 -0700

    test move

--- tools/bin/archive_current_dbt_docs.sh
+++ tools/bin/archive_current_dbt_docs.sh
@@ -4,8 +4,10 @@ trap 'exit' ERR
 file_path=gs://$GCS_BUCKET/index.html
 result=$(gsutil -q stat $file_path || echo 1)
 if [[ $result != 1 ]]; then
-  now=$(date +%s )
-  echo "File exists $now"
+  NOW=$(date +%s)
+  gsutil mv gs://$GCS_BUCKET/index.html gs://$GCS_BUCKET/ts=$NOW/index.html
+  gsutil mv gs://$GCS_BUCKET/catalog.json gs://$GCS_BUCKET/ts=$NOW/catalog.json
+  gsutil mv gs://$GCS_BUCKET/manifest.json gs://$GCS_BUCKET/ts=$NOW/manfest.html
 else
-  echo "File does not exist"
+  echo "dbt docs does not exist"
 fi
