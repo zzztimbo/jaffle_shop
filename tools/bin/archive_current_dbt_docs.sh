82ee85b3a121e4fbbee6c5b745dba04f20458b53
Tim Chan <tim@chan.net>
2022-09-08 17:32:35 -0700

    test ts

--- tools/bin/archive_current_dbt_docs.sh
+++ tools/bin/archive_current_dbt_docs.sh
@@ -4,7 +4,8 @@ trap 'exit' ERR
 file_path=gs://$GCS_BUCKET/index.html
 result=$(gsutil -q stat $file_path || echo 1)
 if [[ $result != 1 ]]; then
-  echo "File exists"
+  now=$(date +%s )
+  echo "File exists $now"
 else
   echo "File does not exist"
 fi
